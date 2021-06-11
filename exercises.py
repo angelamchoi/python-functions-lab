4ca0873fcd41ed484f319fac96c427e50fa91e57
Angela Choi <angelamchoi@gmail.com>
2021-06-11 10:18:05 -0400

    worked on exercise 1

--- exercises.py
+++ exercises.py
@@ -1,16 +1,24 @@
-# Write a function named sum_to that accepts a single integer, n, and returns the sum of the integers from 1 to n.
+# 1. Write a function named sum_to that accepts a single integer, n, and returns the sum of the integers from 1 to n.
 
 # For example:
 
 # sum_to(6)  # returns 21
 # sum_to(10) # returns 55
-# Write a function named largest that takes a list of numbers as an argument and returns the largest number in that list.
+
+def sum_to(num):
+  sum = 0
+  for n in range(1, num + 1):
+    sum += n
+  return sum
+
+# 2. Write a function named largest that takes a list of numbers as an argument and returns the largest number in that list.
 
 # For example:
 
 # largest([1, 2, 3, 4, 0])  # returns 4
 # largest([10, 4, 2, 231, 91, 54])  # returns 231
-# Write a function named occurances that takes two string arguments as input and counts the number of occurances of the second string inside the first string.
+
+#3. Write a function named occurances that takes two string arguments as input and counts the number of occurances of the second string inside the first string.
 
 # For example:
 
@@ -18,7 +26,8 @@
 # occurances('fleep floop', 'p')   # returns 2
 # occurances('fleep floop', 'ee')  # returns 1
 # occurances('fleep floop', 'fe')  # returns 0
-# Write a function named product that takes an arbitrary number of numbers, multiplies them all together, and returns the product.
+
+#4. Write a function named product that takes an arbitrary number of numbers, multiplies them all together, and returns the product.
 # (HINT: Review your notes on *args).
 
 # For example:
